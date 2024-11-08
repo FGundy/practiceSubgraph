afc9cbe0d6c85cf58413871f3ee0c3025985cf80
FabioGunderson <fmg44@scarletmail.rutgers.edu>
2024-11-08 21:02:45 +0000

    Initial commit

---
+++ tests/land-wolf-utils.ts
@@ -0,0 +1,92 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  Approval,
+  MaxTxAmountUpdated,
+  OwnershipTransferred,
+  Transfer
+} from "../generated/LandWolf/LandWolf"
+
+export function createApprovalEvent(
+  owner: Address,
+  spender: Address,
+  value: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("spender", ethereum.Value.fromAddress(spender))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
+  )
+
+  return approvalEvent
+}
+
+export function createMaxTxAmountUpdatedEvent(
+  _maxTxAmount: BigInt
+): MaxTxAmountUpdated {
+  let maxTxAmountUpdatedEvent = changetype<MaxTxAmountUpdated>(newMockEvent())
+
+  maxTxAmountUpdatedEvent.parameters = new Array()
+
+  maxTxAmountUpdatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "_maxTxAmount",
+      ethereum.Value.fromUnsignedBigInt(_maxTxAmount)
+    )
+  )
+
+  return maxTxAmountUpdatedEvent
+}
+
+export function createOwnershipTransferredEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
+    newMockEvent()
+  )
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
+
+export function createTransferEvent(
+  from: Address,
+  to: Address,
+  value: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
+  )
+
+  return transferEvent
+}
